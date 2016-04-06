d601d4c076603f175f0ee744f144aacb77b4447a
叶慧伟 <yehuiwei@yehuiweidemacbook-pro.local>
2016-04-06 11:01:46 +0800

    Initial Commit

---
+++ 日历/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  日历
+//
+//  Created by 叶慧伟 on 16/4/6.
+//  Copyright © 2016年 叶慧伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 日历/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  日历
+//
+//  Created by 叶慧伟 on 16/4/6.
+//  Copyright © 2016年 叶慧伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
